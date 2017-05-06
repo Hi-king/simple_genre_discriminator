63931f23e2ec946a76146e358359846244b4c3f8
Hi-king <hikingko1@gmail.com>
2017-05-06 19:29:50 +0900

    :star: predict

---
+++ predict.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import argparse
+import keras.preprocessing.image
+import numpy
+
+parser = argparse.ArgumentParser()
+parser.add_argument('model')
+parser.add_argument('image')
+args = parser.parse_args()
+
+image = keras.preprocessing.image.load_img(
+    args.image,
+    target_size=(224, 224)
+)
+x = numpy.expand_dims(
+    keras.preprocessing.image.img_to_array(image),
+    axis=0
+)
+
+model = keras.models.load_model(args.model)
+print(model.predict(x))
